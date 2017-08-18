a7b9f01310e8a1db883bc05195150508dec27037
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-08-18 13:56:59 +0200

    Add ToDoItem class

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,33 @@
-/**
- * Class representing a single todo item.
- * 
- */
 public class TodoItem{
 
+
+    private String title;
+    private boolean done;
+
+    public ToDoItem(String title, boolean done){
+        this.title = title;
+        this.done = done;
+    }
+
+    public String getTitleAndStatus() {
+
+        String status = this.checkStatus();
+        String fullInfo = status + this.title;
+
+        return (fullInfo);
+    }
+    public String checkStatus() {
+
+        String status;
+        if (this.done) {
+            status = "[X]";
+        } else {
+            status = "[ ]";
+        }
+        return (status);
+    }
+
+    public void markAsDone() {
+        this.done = true;
+    }
 }
